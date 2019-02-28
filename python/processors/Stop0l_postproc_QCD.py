54a0b061901a5f83a8f03f58cd86e8c25d805b79
Matthew <mkilpatr@FNAL.GOV>
2019-02-28 15:54:34 -0600

    make one file for responce and hadd qcd smearing file in background

--- python/processors/Stop0l_postproc_QCD.py
+++ python/processors/Stop0l_postproc_QCD.py
@@ -10,7 +10,6 @@ mods = [
     qcdSmearProducer(),
 ]
 
-#files=["/eos/uscms/store/user/lpcsusyhad/Stop_production/Summer16_80X_v2_NanAOD_MC/PostProcess_v1/QCD_HT100to200/QCD_HT100to200_0.root"]
-files=["/uscms_data/d3/lpcsusyhad/benwu/Moriond2019/TestNanoAOD/CMSSW_10_4_X_2018-12-11-2300/src/prod2017MC_NANO.root"]
+files=["/eos/uscms/store/user/lpcsusyhad/Stop_production/Summer16_80X_v2_NanAOD_MC/PostProcess_v1/QCD_HT100to200/QCD_HT100to200_0.root"]
 p=PostProcessor(".",files,cut=None, branchsel=None, haddFileName=True,outputbranchsel="keep_and_drop_QCD.txt", outputbranchselsmear="keep_and_drop_smear.txt",typeofprocess="smear",modules=mods,provenance=False)
 p.run()
